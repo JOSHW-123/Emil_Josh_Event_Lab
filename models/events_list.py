63f81f974f007cd8441ede5888fd08bad407d143
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2021-07-01 14:22:24 +0100

    First cpmmit

---
+++ models/events_list.py
@@ -0,0 +1,8 @@
+from models.event import *
+
+event1 = Event("01.07.2021", "Eminem", 1000, "SSE Hydro", "Concert")
+event2 = Event("10.07.2021", "James Bond",20, "Cineworld", "Cinema")
+events = [event1, event2]
+
+def add_new_event(event):
+    events.append(event)
